749f8cb51d98b048ef941cd937645165e398fe8d
LukasPio <lucaspio.galvao@gmail.com>
2024-10-22 12:04:43 -0300

    add javascript to show correct answer of some questions

---
+++ javascript/modelDataNotations.js
@@ -0,0 +1,15 @@
+document.getElementById("submit").onclick = function () {
+  if (document.getElementById("question1-A").checked) {
+    document.getElementById("correctOption").textContent =
+      "Parabéns, essa era a opção correta!";
+  } else if (
+    document.getElementById("question1-B").checked ||
+    document.getElementById("question1-C").checked
+  ) {
+    document.getElementById("correctOption").textContent =
+      "Boa tentativa mas essa não era a opção correta!";
+  } else {
+    document.getElementById("correctOption").textContent =
+      "Selecione uma alternativa!";
+  }
+};
